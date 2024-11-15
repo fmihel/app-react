871c48333d523176f21e731f5961ed6333b4d7c5
fmihel <fmihel76@gmail.com>
2024-11-15 10:22:36 +0300

    beta

--- app/index.js
+++ app/index.js
@@ -1,16 +1,14 @@
 import redux from 'REDUX';
-// import './lazy-load.config';
-// import { loadCSS } from 'fmihel-lazy-load';
-import './actions';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App.jsx';
+import './actions';
 import './index.scss';
+import App from './App.jsx';
 
-// loadCSS('style/index.css');
 
 redux.on(() => {
     console.log('change state', redux.getState());
 });
+
 createRoot(document.getElementById('root')).render(<Provider store={redux.store}><App /></Provider>);
